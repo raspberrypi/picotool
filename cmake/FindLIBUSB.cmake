1f504efee2e9675e53b267d846147c0b4cdb6697
micropicostack <andre.leppik@1acue.de>
2024-07-19 17:00:24 +0300

    fix: Improper check for the PkgConfig tool usage

--- cmake/FindLIBUSB.cmake
+++ cmake/FindLIBUSB.cmake
@@ -15,8 +15,9 @@ else (LIBUSB_INCLUDE_DIR AND LIBUSB_LIBRARIES)
     # use pkg-config to get the directories and then use these values
     # in the find_path() and find_library() calls. Might fail, pkg-config
     # might not be installed, e.g. for Windows systems
+    find_package(PkgConfig)
+
     if (PKG_CONFIG_FOUND)
-        find_package(PkgConfig)
         pkg_check_modules(PC_LIBUSB libusb-1.0)
     else ()
         # As the pkg-config was not found we are probably building under windows.
@@ -29,18 +30,22 @@ else (LIBUSB_INCLUDE_DIR AND LIBUSB_LIBRARIES)
             endif()
         endif()
 
-        find_path(LIBUSB_INCLUDE_DIR libusb.h
-            HINTS $ENV{LIBUSB_ROOT}/include/libusb-1.0
-            PATHS ${PC_LIBUSB_INCLUDEDIR} ${PC_LIBUSB_INCLUDE_DIRS})
+        set(PC_LIBUSB_INCLUDEDIR_HINT $ENV{LIBUSB_ROOT}/include/libusb-1.0)
 
-        find_library(LIBUSB_LIBRARIES NAMES libusb-1.0 usb-1.0 usb
-            if (MINGW OR CYGWIN)
-                HINTS $ENV{LIBUSB_ROOT}/MinGW${ARCHITECTURE}/static
-            elseif(MSVC)
-                HINTS $ENV{LIBUSB_ROOT}/VS2019/MS${ARCHITECTURE}/static
-            endif ()
-            PATHS ${PC_LIBUSB_LIBDIR} ${PC_LIBUSB_LIBRARY_DIRS})
+        if (MINGW OR CYGWIN)
+            set(PC_LIBUSB_LIBDIR_HINT $ENV{LIBUSB_ROOT}/MinGW${ARCHITECTURE}/static)
+        elseif(MSVC)
+            set(PC_LIBUSB_LIBDIR_HINT $ENV{LIBUSB_ROOT}/VS2019/MS${ARCHITECTURE}/static)
+        endif ()
     endif ()
+
+    find_path(LIBUSB_INCLUDE_DIR libusb.h
+        HINTS ${PC_LIBUSB_INCLUDEDIR_HINT}
+        PATHS ${PC_LIBUSB_INCLUDEDIR} ${PC_LIBUSB_INCLUDE_DIRS})
+        
+        find_library(LIBUSB_LIBRARIES NAMES libusb-1.0 usb-1.0 usb
+        HINTS ${PC_LIBUSB_LIBDIR_HINT}
+        PATHS ${PC_LIBUSB_LIBDIR} ${PC_LIBUSB_LIBRARY_DIRS})
 
     include(FindPackageHandleStandardArgs)
 
