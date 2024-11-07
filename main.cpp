dc9b5494fe78e3fe52fd11d7427d2be72e55f37a
will-v-pi <108662275+will-v-pi@users.noreply.github.com>
2024-11-07 14:37:08 +0000

    Always print serial number when unable to find a device with specific serial number (#164)
    Previously it would not print the serial number if a device wasn't found after a -f reboot (see https://forums.raspberrypi.com/viewtopic.php?t=378682)

--- main.cpp
+++ main.cpp
@@ -3581,7 +3581,7 @@ string missing_device_string(bool wasRetry, bool requires_rp2350 = false) {
         }
     } else if (settings.bus != -1) {
         snprintf(buf, buf_len, "accessible %s devices in BOOTSEL mode were found found on bus %d.", device_name, settings.bus);
-    } else if (!settings.ser.empty() && !wasRetry) {
+    } else if (!settings.ser.empty()) {
         snprintf(buf, buf_len, "accessible %s devices in BOOTSEL mode were found found with serial number %s.", device_name, settings.ser.c_str());
     } else {
         snprintf(buf, buf_len, "accessible %s devices in BOOTSEL mode were found.", device_name);
