ba574e800fe6d121898c0ec0d3a02087ffca1e1c
William Vinnicombe <william.vinnicombe@raspberrypi.com>
2024-12-19 14:06:22 +0000

    Fix support for CircuitPython block device (Fat) and add full Flash Translation Layer (prevents doing lots of erases)
    Change Fat sector size back to 512, and consequently add in flash translation error - wasn't needed before with 4096 sector size
    
    Also enable LFN support

--- lib/oofatfs/src/ffconf.h
+++ lib/oofatfs/src/ffconf.h
@@ -105,7 +105,7 @@
 */
 
 
-#define FF_USE_LFN      0
+#define FF_USE_LFN      1
 #define FF_MAX_LFN      255
 /* The FF_USE_LFN switches the support for LFN (long file name).
 /
@@ -198,8 +198,8 @@
 /  funciton will be available. */
 
 
-#define FF_MIN_SS       4096
-#define FF_MAX_SS       4096
+#define FF_MIN_SS       512
+#define FF_MAX_SS       512
 /* This set of options configures the range of sector size to be supported. (512,
 /  1024, 2048 or 4096) Always set both 512 for most systems, generic memory card and
 /  harddisk. But a larger value may be required for on-board flash memory and some
@@ -208,7 +208,7 @@
 /  GET_SECTOR_SIZE command. */
 
 
-#define FF_USE_TRIM     0
+#define FF_USE_TRIM     1
 /* This option switches support for ATA-TRIM. (0:Disable or 1:Enable)
 /  To enable Trim function, also CTRL_TRIM command should be implemented to the
 /  disk_ioctl() function. */
