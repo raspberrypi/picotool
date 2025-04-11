3054626a9eddc34ca96f3c32ff0573e5027a58e5
William Vinnicombe <william.vinnicombe@raspberrypi.com>
2025-04-11 13:25:43 +0100

    Switch to pico-sdk style mbedtls library

--- lib/include/pico_mbedtls_config.h
+++ lib/include/pico_mbedtls_config.h
@@ -4199,19 +4199,6 @@
 
 /** \} name SECTION: Module configuration options */
 
-/* Target and application specific configurations
- *
- * Allow user to override any previous default.
- *
- */
-#if defined(MBEDTLS_USER_CONFIG_FILE)
-#include MBEDTLS_USER_CONFIG_FILE
-#endif
-
-#if defined(MBEDTLS_PSA_CRYPTO_CONFIG)
-#include "mbedtls/config_psa.h"
-#endif
-
-#include "mbedtls/check_config.h"
+#include <limits.h>
 
 #endif /* MBEDTLS_CONFIG_H */
