235ce6e2e7dc51140d1898e4de32a3ada11aa4ca
William Vinnicombe <william.vinnicombe@raspberrypi.com>
2025-06-18 12:06:48 +0100

    Tidy up file checks

--- .github/workflows/check_precompiled.sh
+++ .github/workflows/check_precompiled.sh
@@ -3,15 +3,13 @@
 # Pass updated files in $updated_files environment variable
 
 # enc_bootloader
-echo "$updated_files" | grep -q "^enc_bootloader/.*\.elf$"
-enc_bootloader_elf_not_updated=$?
-if [ $enc_bootloader_elf_not_updated -eq 1 ]; then
+if ! echo "$updated_files" | grep -q "^enc_bootloader/.*\.elf$"; then
     echo "Checking enc_bootloader files for modifications as ELFs have not been updated"
     for file in enc_bootloader/*; do
         if [[ "$file" == "enc_bootloader/CMakeLists.txt" || "$file" == "enc_bootloader/BUILD.bazel" ]]; then
             continue
         fi
-        if echo "$updated_files" | grep -q "$file"; then
+        if [[ "$updated_files" == *"$file"* ]]; then
             echo "File $file is in the PR but enc_bootloader ELFs have not been updated"
             exit 1
         fi
@@ -19,15 +17,13 @@ if [ $enc_bootloader_elf_not_updated -eq 1 ]; then
 fi
 
 # picoboot_flash_id
-echo "$updated_files" | grep -q "^picoboot_flash_id/.*\.bin$"
-flash_id_bin_not_updated=$?
-if [ $flash_id_bin_not_updated -eq 1 ]; then
+if ! echo "$updated_files" | grep -q "^picoboot_flash_id/.*\.bin$"; then
     echo "Checking picoboot_flash_id files for modifications as BINs have not been updated"
     for file in picoboot_flash_id/*; do
         if [[ "$file" == "picoboot_flash_id/CMakeLists.txt" || "$file" == "picoboot_flash_id/BUILD.bazel" ]]; then
             continue
         fi
-        if echo "$updated_files" | grep -q "$file"; then
+        if [[ "$updated_files" == *"$file"* ]]; then
             echo "File $file is in the PR but flash_id BINs have not been updated"
             exit 1
         fi
@@ -35,15 +31,13 @@ if [ $flash_id_bin_not_updated -eq 1 ]; then
 fi
 
 # xip_ram_perms
-echo "$updated_files" | grep -q "^xip_ram_perms/.*\.elf$"
-xip_ram_perms_elf_not_updated=$?
-if [ $xip_ram_perms_elf_not_updated -eq 1 ]; then
+if ! echo "$updated_files" | grep -q "^xip_ram_perms/.*\.elf$"; then
     echo "Checking xip_ram_perms files for modifications as ELFs have not been updated"
     for file in xip_ram_perms/*; do
         if [[ "$file" == "xip_ram_perms/CMakeLists.txt" || "$file" == "xip_ram_perms/BUILD.bazel" ]]; then
             continue
         fi
-        if echo "$updated_files" | grep -q "$file"; then
+        if [[ "$updated_files" == *"$file"* ]]; then
             echo "File $file is in the PR but xip_ram_perms ELFs have not been updated"
             exit 1
         fi
